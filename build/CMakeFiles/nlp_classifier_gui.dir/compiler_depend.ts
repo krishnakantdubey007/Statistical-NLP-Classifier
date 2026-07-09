affbfbde861ff725f9da2e4816c30bac5e6d8e99
Krishnakant Dubey <krishnakantdubey338@gemail.com>
2026-07-09 11:21:29 +0530

    Remove build directory from repository

--- build/CMakeFiles/nlp_classifier_gui.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for nlp_classifier_gui.
--- build/CMakeFiles/nlp_classifier_gui.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for nlp_classifier_gui.
-# This may be replaced when dependencies are built.
